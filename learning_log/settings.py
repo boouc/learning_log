3f5253c024651590e5e7597b7feb02e962d1c884
boouc <33542657+boouc@users.noreply.github.com>
2018-11-19 14:52:57 +0800

    关闭heroku的DEBUG.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -142,7 +142,9 @@ if os.getcwd() == "/app":
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
     # 支持所有的主机头(host header)
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ["ll-note.herokuapp.com"]
+
+    DEBUG = False
 
     # 静态资源配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
